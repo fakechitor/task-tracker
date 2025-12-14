807d0afbb3c44337746463aa82f653b9ebbf7e0b
fakechitor <ladasedanbaklajan06@gmail.com>
2025-12-14 06:42:29 +0300

    fix(backend): fix user model

--- task-tracker-backend/src/main/java/com/fakechitor/tasktrackerbackend/model/User.java
+++ task-tracker-backend/src/main/java/com/fakechitor/tasktrackerbackend/model/User.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @Entity
+@Table(name = "users")
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

8e1d8163e96c07586d67ddc86198c185ef899758
fakechitor <ladasedanbaklajan06@gmail.com>
2025-12-16 10:32:25 +0300

    fix(backend): change interface

--- task-tracker-backend/src/main/java/com/fakechitor/tasktrackerbackend/service/AuthenticationService.java
+++ task-tracker-backend/src/main/java/com/fakechitor/tasktrackerbackend/service/AuthenticationService.java
@@ -16,7 +16,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
-    private final UserServiceImpl userService;
+    private final UserService userService;
 
     private final JwtService jwtService;
 

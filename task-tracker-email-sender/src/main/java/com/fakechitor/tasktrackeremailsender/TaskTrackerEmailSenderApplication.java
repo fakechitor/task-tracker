3fae495449fd3e784d11a94fc45474c758576818
fakechitor <ladasedanbaklajan06@gmail.com>
2025-12-17 02:35:36 +0300

    feat(email): initialize project

---
+++ task-tracker-email-sender/src/main/java/com/fakechitor/tasktrackeremailsender/TaskTrackerEmailSenderApplication.java
@@ -0,0 +1,13 @@
+package com.fakechitor.tasktrackeremailsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerEmailSenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskTrackerEmailSenderApplication.class, args);
+    }
+
+}
